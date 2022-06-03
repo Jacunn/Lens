e0c3c47e0114ab41244a4123ef40356e9ad54c3d
jesse <jesse.cunningham.1998@gmail.com>
2022-06-03 18:34:18 +1000

    [Change] Cleaning up code.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
-        // Constants for inputs shared across functions. These are kept short for the purposes of generating shorter links.
+    // Constants for inputs shared across functions. These are kept short for the purposes of generating shorter links.
     private $param_names = [
         'where-column'      => 'a',
         'where-condition'   => 'b',
@@ -52,8 +52,8 @@ class Controller extends BaseController
         $accessible_databases = Auth::user()->databasesAccessible();
 
         foreach($database_results as $database) {
-            // Check if the user has access to that database.
-            if(isset($accessible_databases[$database->Database])) {
+            // Check if the user has access to that database, or no access is required.
+            if(isset($accessible_databases[$database->Database]) || config('lens.require_permission')) {
                 // Check if an alias for this database exists - If so, use that. If not, use the default name.
                 $database_list[] = [
                     'alias' => isset($database_aliases[$database->Database]) ? $database_aliases[$database->Database]['alias'] : $database->Database, 
@@ -72,71 +72,76 @@ class Controller extends BaseController
         $database_input = $_request->input($this->param_names['database-name']);
         $database_exists = count(DB::select('SHOW DATABASES WHERE `Database` LIKE ?', [$database_input]));
 
-        // Check whether we're being provided a valid database - If not, consider this inappropriate meddling.
-        if($database_exists !== 1)
+        // Check whether we're being provided a valid database - If not, redirect the user to the database selection page.
+        if($database_exists)
         {
-            die('HIGH LEVEL INJECTION ATTEMPT OR FALSE DATABASE PROVIDED. BOOTING USER AND TERMINATING PROCESS.');
-        }
+            // Get table and column aliases for a given database so we're only querying this once...
+            $table_aliases = TableAlias::where('database_name', '=', $this->param_names['database-name'])->get()->keyBy('table_name');
+            $column_aliases = ColumnAlias::where('database_name', '=', $this->param_names['database-name'])->get()->keyBy(function($x) {
+                return "$x->table_name.$x->column_name";
+            });
 
-        // Get table and column aliases for a given database so we're only querying this once...
-        $table_aliases = TableAlias::where('database_name', '=', $this->param_names['database-name'])->get()->keyBy('table_name');
-        $column_aliases = ColumnAlias::where('database_name', '=', $this->param_names['database-name'])->get()->keyBy(function($x) {
-            return "$x->table_name.$x->column_name";
-        });
+            $table_results = DB::select("SHOW FULL TABLES FROM $database_input WHERE Table_Type NOT LIKE 'VIEW'");
+            $table_list = [];
 
-        $table_results = DB::select("SHOW FULL TABLES FROM $database_input WHERE Table_Type NOT LIKE 'VIEW'");
-        $table_list = [];
+            // Select all of the tables accessible to the user...
+            $accessible_tables = Auth::user()->tablesAccessible($database_input);
 
-        // Select all of the tables accessible to the user...
-        $accessible_tables = Auth::user()->tablesAccessible($database_input);
+            foreach($table_results as $table) {
+                // Extract the name from our above query results...
+                $table_name = $table->{"Tables_in_$database_input"};
 
-        foreach($table_results as $table) {
-            // Extract the name from our above query results...
-            $table_name = $table->{"Tables_in_$database_input"};
+                // Check if the user has permission to access the table, or if access permission is not required.
+                if(isset($accessible_tables[$table_name]) || config('lens.require_permission')) {
+                    // Get a result list of columns for a given table...
+                    $table_description_results = DB::select("DESCRIBE $database_input.$table_name");
 
-            if(isset($accessible_tables[$table_name])) {
-                // Get a result list of columns for a given table...
-                $table_description_results = DB::select("DESCRIBE $database_input.$table_name");
+                    $table_list[$table_name] = [
+                        'table_name' => $table_name,
+                        'table_alias' => isset($table_aliases->$table_name) ? $table_aliases->$table_name->alias : $table_name,
+                        'table_columns' => []
+                    ];
 
-                $table_list[$table_name] = [
-                    'table_name' => $table_name,
-                    'table_alias' => isset($table_aliases->$table_name) ? $table_aliases->$table_name->alias : $table_name,
-                    'table_columns' => []
-                ];
-
-                // Iterate across those columns and produce a model of how the data is going to look...
-                foreach($table_description_results as $column) {
-                    $table_list[$table_name]['table_columns'][$column->Field] = [
-                        'name' => $column->Field,
-                        'alias' => isset($column_aliases->{"$table_name.$column->Field"}) ? $column_aliases->{"$table_name.$column->Field"}->alias : $column->Field,
-                        'type' => $this->process_type($column->Type)
-                    ]; 
+                    // Iterate across those columns and produce a model of how the data is going to look...
+                    foreach($table_description_results as $column) {
+                        $table_list[$table_name]['table_columns'][$column->Field] = [
+                            'name' => $column->Field,
+                            'alias' => isset($column_aliases->{"$table_name.$column->Field"}) ? $column_aliases->{"$table_name.$column->Field"}->alias : $column->Field,
+                            'type' => $this->process_type($column->Type)
+                        ]; 
+                    }
                 }
             }
-        }
 
-        return view('page.query-crafter', [
-            'tables' => $table_list,
-            'database' => $database_input,
-            'back_link' => route('database-select'),
-            'param_names' => $this->param_names
-        ]);
+            return view('page.query-crafter', [
+                'tables' => $table_list,
+                'database' => $database_input,
+                'back_link' => route('database-select'),
+                'param_names' => $this->param_names
+            ]);
+        }
     }
 
     function page_table_view(Request $_request) {
-        // Build simple model of the query to pass on to the end user...
-        $query = DB::table($_request->input($this->param_names['database-name']) . '.' . $_request->input($this->param_names['table-name']));
-
-        // Build up our where conditions for insertion.
-        $conditions = [];
-
+        // The database, table and columns we're returning from.
+        $database_name = $_request->input($this->param_names['database-name']);
+        $table_name = $_request->input($this->param_names['table-name']);
+        $column_names = $_request->input($this->param_names['select-column']);
+                
+        // The filtering we're performing against the database.
         $where_columns = $_request->input($this->param_names['where-column']);
         $where_conditions = $_request->input($this->param_names['where-condition']);
         $where_inputs = $_request->input($this->param_names['where-value']);
 
+        // Build simple model of the query to pass on to the end user...
+        $query = DB::table("$database_name.$table_name");
+
+        // Build up our where conditions for insertion.
+        $conditions = [];
+
         if($where_columns !== null) {
             for($i = count($where_columns) - 1; $i >= 0; --$i) {
-                if($this->valid_condition($_request->input($this->param_names['database-name']), $_request->input($this->param_names['table-name']), $where_columns[$i], $where_conditions[$i])) {
+                if($this->valid_condition($database_name, $table_name, $where_columns[$i], $where_conditions[$i])) {
                     
                     $conditions[] = $this->translate_condition($where_columns[$i], $where_conditions[$i], $where_inputs[$i]);
                 }
@@ -147,7 +152,6 @@ class Controller extends BaseController
         $query = $query->where($conditions);
 
         // Generate our columns
-        $columns = $_request->input($this->param_names['select-column']);
         $column_aliases = ColumnAlias::where([
             ['database_name', '=', $this->param_names['database-name']],
             ['table_name', '=', $this->param_names['table-name']]
@@ -156,68 +160,84 @@ class Controller extends BaseController
         return view('page.table-view', [
             'get_parameters' => $_request->all(),
             'query_sql' => $query->toSql(),
-            'columns' => $columns,
+            'columns' => $column_names,
             'column_aliases' => $column_aliases,
             'copy_link' => route('table-view', $_request->all()),
-            'back_link' => route('query-crafter', [$this->param_names['database-name'] => $_request->input($this->param_names['database-name'])]),
+            'back_link' => route('query-crafter', [$database_name => $database_name]),
             'param_names' => $this->param_names
         ]);
     }
 
     function callback_table_csv(Request $_request) {
-        // Reserve our query object for building.
-        $query = DB::table($_request->input($this->param_names['database-name']) . '.' . $_request->input($this->param_names['table-name']));
+        $database_name = $_request->input($this->param_names['database-name']);
+        $table_name = $_request->input($this->param_names['table-name']);
+        $column_names = $_request->input($this->param_names['select-column']);
 
-        // Build up our where conditions for insertion.
-        $conditions = [];
+        if(Auth::user()->tablesAccessible($database_name) && Auth::user()->tablesAccessible($table_name)) {
+            // Reserve our query object for building.
+            $query = DB::table("$database_name.$table_name");
 
-        $where_columns = $_request->input($this->param_names['where-column']);
-        $where_conditions = $_request->input($this->param_names['where-condition']);
-        $where_inputs = $_request->input($this->param_names['where-value']);
+            // Build up our where conditions for insertion.
+            $conditions = [];
 
-        if($where_columns !== null) {
-            for($i = count($where_columns) - 1; $i >= 0; --$i) {
-                if($this->valid_condition($_request->input($this->param_names['database-name']), $_request->input($this->param_names['table-name']), $where_columns[$i], $where_conditions[$i])) {
-                    
-                    $conditions[] = $this->translate_condition($where_columns[$i], $where_conditions[$i], $where_inputs[$i]);
+            $where_columns = $_request->input($this->param_names['where-column']);
+            $where_conditions = $_request->input($this->param_names['where-condition']);
+            $where_inputs = $_request->input($this->param_names['where-value']);
+
+            if($where_columns !== null) {
+                for($i = count($where_columns) - 1; $i >= 0; --$i) {
+                    if($this->valid_condition($database_name, $table_name, $where_columns[$i], $where_conditions[$i])) {
+                        
+                        $conditions[] = $this->translate_condition($where_columns[$i], $where_conditions[$i], $where_inputs[$i]);
+                    }
                 }
             }
-        }
 
-        // Put in our pagination and attach the where conditions.
-        $results = $query->where($conditions)->get();
+            // Put in our pagination and attach the where conditions.
+            $results = $query->where($conditions)->get();
 
-        // Filter for our desired columns.
-        $product = [];
-        $product[] = $_request->input($this->param_names['select-column']);
-    
-        foreach($results as $row) {
-            $filtered_row = [];
-            foreach($_request->input($this->param_names['select-column']) as $column) {
-                $filtered_row[] = $row->$column;
+            // Filter for our desired columns.
+            $product = [];
+
+            foreach($results as $row) {
+                $filtered_row = [];
+                foreach($column_names as $column) {
+                    $filtered_row[] = $row->$column;
+                }
+
+                $product[] = $filtered_row;
             }
 
-            $product[] = $filtered_row;
+            // Convert it into data-tables format.    
+            return view('export.csv', [ 'rows' => $product ]);
         }
-
-        // Convert it into data-tables format.    
-        return view('export.csv', [ 'rows' => $product ]);
     }
 
     function callback_table_view(Request $_request) {
-        // Reserve our query object for building.
-        $filtered_query = DB::table($_request->input($this->param_names['database-name']) . '.' . $_request->input($this->param_names['table-name']));
-
-        // Build up our where conditions for insertion.
-        $conditions = [];
-
+        // The database, table and columns we're returning from.
+        $select_database = $_request->input($this->param_names['database-name']);
+        $select_table = $_request->input($this->param_names['table-name']);
+        $select_columns = $_request->input($this->param_names['select-column']);
+        
+        // The filtering we're performing against the database.
         $where_columns = $_request->input($this->param_names['where-column']);
         $where_conditions = $_request->input($this->param_names['where-condition']);
         $where_inputs = $_request->input($this->param_names['where-value']);
 
+        // Additional filtering for pagination and searching.
+        $skip_rows = $_request->input($this->param_names['dt-start']) ?? 0;
+        $take_rows = $_request->input($this->param_names['dt-length']) ?? 100;
+        $search_criteria = $_request->input($this->param_names['dt-search'])[$this->param_names['dt-search-value']];
+
+        // Reserve our query object for building.
+        $filtered_query = DB::table("$select_database.$select_table");
+
+        // Build up our where conditions for insertion.
+        $conditions = [];
+
         if($where_columns !== null) {
             for($i = count($where_columns) - 1; $i >= 0; --$i) {
-                if($this->valid_condition($_request->input($this->param_names['database-name']), $_request->input($this->param_names['table-name']), $where_columns[$i], $where_conditions[$i])) {
+                if($this->valid_condition($select_database, $select_table, $where_columns[$i], $where_conditions[$i])) {
                     
                     $conditions[] = $this->translate_condition($where_columns[$i], $where_conditions[$i], $where_inputs[$i]);
                 }
@@ -225,24 +245,23 @@ class Controller extends BaseController
         }
 
         // Put in our pagination and attach the where conditions.
-        $filtered_query = $filtered_query->where($conditions)->where(function ($x) use($_request) {
+        $filtered_query = $filtered_query->where($conditions)->where(function ($x) use($select_columns, $search_criteria) {
             // Generate additional where criteria if search conditions are supplied.
-            if($_request->input($this->param_names['dt-search'])[$this->param_names['dt-search-value']] !== null) {
-                for($i = count($_request->input($this->param_names['select-column'])) - 1; $i >= 0; --$i) {
-                    $x->orWhere($_request->input($this->param_names['select-column'])[$i], 'LIKE', '%' . $_request->input($this->param_names['dt-search'])[$this->param_names['dt-search-value']] . '%');
+            if($search_criteria !== null) {
+                for($i = count($select_columns) - 1; $i >= 0; --$i) {
+                    $x->orWhere($select_columns[$i], 'LIKE', "%$search_criteria%");
                 }
             }
-
         });
 
         // Now get the total of records there are.
-        $total = DB::table($_request->input($this->param_names['database-name']) . '.' . $_request->input($this->param_names['table-name']))->count();
+        $total = DB::table("$select_database.$select_table")->count();
         
         // Get the total that meet our filter criteria.
         $filtered = $filtered_query->count();
 
         // Execute the query.
-        $full_query = $filtered_query->skip($_request->input($this->param_names['dt-start']) ?? 0)->take($_request->input($this->param_names['dt-length']) ?? 100);
+        $full_query = $filtered_query->skip($skip_rows)->take($take_rows);
         $results = $full_query->get();
 
         // Filter for our desired columns.
@@ -250,7 +269,7 @@ class Controller extends BaseController
 
         foreach($results as $row) {
             $filtered_row = [];
-            foreach($_request->input($this->param_names['select-column']) as $column) {
+            foreach($select_columns as $column) {
                 $filtered_row[$column] = $row->$column;
             }
 
